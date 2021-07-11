9da0f9bc5a0ecacbbf5d6543209b9ade4df5ec56
FEDEDAM <federico.asir@gmail.com>
2021-07-11 19:49:01 +0200

    init api-rest project

---
+++ backend/api-rest/app_citas_medicas/src/main/java/com/fedejcs/spring/apirest/citasmedicas/app/app_citas_medicas/AppCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.fedejcs.spring.apirest.citasmedicas.app.app_citas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCitasMedicasApplication.class, args);
+	}
+
+}
---
+++ backend/api-rest/app_citas_medicas/src/test/java/com/fedejcs/spring/apirest/citasmedicas/app/app_citas_medicas/AppCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedejcs.spring.apirest.citasmedicas.app.app_citas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
